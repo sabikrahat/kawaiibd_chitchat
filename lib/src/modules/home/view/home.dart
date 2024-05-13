f0c54b4ad6e6bd881cffe323c11ecf3b1d1e33c7
Md. Sabik Alam Rahat <sabikrahat72428@gmail.com>
2024-05-13 23:32:17 +0600

    flutter_hook package added for optimization

--- lib/src/modules/home/view/home.dart
+++ lib/src/modules/home/view/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../../app.routes.dart';
@@ -113,7 +114,7 @@ class _Body extends ConsumerWidget {
   }
 }
 
-class _NoChatsWidget extends StatelessWidget {
+class _NoChatsWidget extends HookWidget {
   const _NoChatsWidget();
 
   @override
